7e94b6d3f8816fa4911f14fbde1cbe4830de986d
DESKTOP-MTLSJG5\Win10 <bluevnd@gmail.com>
2021-11-05 08:49:52 +0700

    Add project files.

---
+++ Bookstore/Controllers/BooksController.cs
@@ -0,0 +1,59 @@
+﻿using Bookstore.Models;
+using Bookstore.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+        [HttpGet]
+        public ActionResult<List<Book>> GetAll() => _bookService.GetAll();
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> GetById(string id)
+        {
+            var book = _bookService.GetById(id);
+            if (book == null) return NotFound();
+            return book;
+        }
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookService.Create(book);
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookService.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _bookService.Update(id, bookIn);
+            return NoContent();
+        }
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _bookService.Remove(book);
+            return NoContent();
+        }
+    }
+}
---
+++ Bookstore/Services/BookService.cs
@@ -0,0 +1,32 @@
+﻿using Bookstore.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+        public List<Book> GetAll() => _books.Find(x => true).ToList();
+        public Book GetById(string id) => _books.Find(x => x.Id == id).FirstOrDefault();
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+        public void Update(string id, Book bookIn) =>
+            _books.ReplaceOne(x => x.Id == id, bookIn);
+        public void Remove(string id) => _books.DeleteOne(x => x.Id == id);
+        public void Remove(Book book) => _books.DeleteOne(x => x.Id == book.Id);
+    }
+}
